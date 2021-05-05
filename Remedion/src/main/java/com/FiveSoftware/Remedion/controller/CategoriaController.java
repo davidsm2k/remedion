09c072a3a4eacd5b341a9b124e83f7a797d76140
Paty-star <80486451+Paty-star@users.noreply.github.com>
2021-05-05 13:54:38 -0300

    Criação crud Categoria

---
+++ Remedion/src/main/java/com/FiveSoftware/Remedion/controller/CategoriaController.java
@@ -0,0 +1,59 @@
+package com.FiveSoftware.Remedion.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.FiveSoftware.Remedion.model.Categoria;
+import com.FiveSoftware.Remedion.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*",allowedHeaders = "*")
+public class CategoriaController {
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> findAllCategoria(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> findByIdCategoria(@PathVariable long id){
+		return repository.findById(id).map(res-> ResponseEntity.ok(res)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> postCategoria(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Categoria> putCategoria(@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteByIdCategoria(@PathVariable long id ) {
+		repository.deleteById(id);
+		
+	}
+	
+	@GetMapping("/tipo/{tipo}")
+	public ResponseEntity<List<Categoria>>findByTipoCategoria(@PathVariable String tipo){
+		return ResponseEntity.ok(repository.findAllByTipoContainingIgnoreCase(tipo));
+	}
+}
