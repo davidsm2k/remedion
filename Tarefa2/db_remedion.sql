b20c544ec6dc55240d6faecbbccf536abf5b2c32
Jullyane <jullyanedmt@gmail.com>
2021-05-12 11:23:33 -0300

    atualização do arquivo sql

--- Tarefa2/db_remedion.sql
+++ Tarefa2/db_remedion.sql
@@ -2,33 +2,33 @@ CREATE DATABASE db_remedion;
 
 USE db_remedion;
 
-CREATE TABLE `tb_usuario` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
-	`nome_completo` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`senha` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE tb_usuario (
+	id bigint NOT NULL AUTO_INCREMENT,
+	nome_completo varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	senha varchar(255) NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE `tb_categoria` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
-	`tipo` varchar(255) NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE tb_categoria (
+	id bigint NOT NULL AUTO_INCREMENT,
+	tipo varchar(255) NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE `tb_produto` (
-	`id` bigint NOT NULL AUTO_INCREMENT,
-	`categoria_id` bigint NOT NULL,
-	`nome` varchar(255) NOT NULL,
-	`estoque` int NOT NULL,
-	`posto` varchar(255) NOT NULL,
-	`municipio_cidade` varchar(255) NOT NULL,
-	`zona` varchar(255) NOT NULL,
-	`endereco` varchar(255) NOT NULL,
-	`classificacao` varchar(255) NOT NULL,
-	`ativo` BOOLEAN NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE tb_produto (
+	id bigint NOT NULL AUTO_INCREMENT,
+	categoria_id bigint NOT NULL,
+	nome varchar(255) NOT NULL,
+	estoque int NOT NULL,
+	posto varchar(255) NOT NULL,
+	municipio_cidade varchar(255) NOT NULL,
+	zona varchar(255) NOT NULL,
+	endereco varchar(255) NOT NULL,
+	classificacao varchar(255) NOT NULL,
+	ativo BOOLEAN NOT NULL,
+	PRIMARY KEY (id)
 );
 
-ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria`(`id`);
+ALTER TABLE tb_produto ADD CONSTRAINT tb_produto_fk0 FOREIGN KEY (categoria_id) REFERENCES tb_categoria(id);
 
