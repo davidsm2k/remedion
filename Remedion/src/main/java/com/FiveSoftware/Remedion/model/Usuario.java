29ca48b73bb68ee80ade86f99892396633647330
Jullyane <jullyanedmt@gmail.com>
2021-05-12 10:19:28 -0300

    adicionar camada de segurança na aplicação

--- Remedion/src/main/java/com/FiveSoftware/Remedion/model/Usuario.java
+++ Remedion/src/main/java/com/FiveSoftware/Remedion/model/Usuario.java
@@ -19,7 +19,7 @@ public class Usuario {
 	
 	@NotNull
 	@Size(max = 255)
-	private String nome_completo;
+	private String nomeCompleto;
 	
 	@NotNull
 	@Size(max = 255)
@@ -32,17 +32,18 @@ public class Usuario {
 	public long getId() {
 		return id;
 	}
+	
 	/**
 	public void setId(long id) {
 		this.id = id;
 	}
 	**/
-	public String getNome_completo() {
-		return nome_completo;
+	
+	public String getNomeCompleto() {
+		return nomeCompleto;
 	}
-
-	public void setNome_completo(String nome_completo) {
-		this.nome_completo = nome_completo;
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
 	}
 
 	public String getEmail() {
